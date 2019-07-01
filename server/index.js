731864213bede30a3c57472e9dbc8740f5c0e189
Nicholas Pung <nckpng95@gmail.com>
2019-07-01 09:59:09 -0700

    added stripe api connection and testing charges is working

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys')
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
+require("./models/User");
+require("./services/passport");
 
-mongoose.connect(keys.mongoURI, {useNewUrlParser:true})
+mongoose
+  .connect(
+    keys.mongoURI,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,7 +27,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
